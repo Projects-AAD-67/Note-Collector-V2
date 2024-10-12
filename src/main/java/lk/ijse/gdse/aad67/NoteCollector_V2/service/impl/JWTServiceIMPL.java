5166b15e454812279eb9e64394ce55491979d577
ThanuraSilva <thanurasilva1989@gmail.com>
2024-10-12 15:46:02 +0530

    Handle JWT : extract username

--- src/main/java/lk/ijse/gdse/aad67/NoteCollector_V2/service/impl/JWTServiceIMPL.java
+++ src/main/java/lk/ijse/gdse/aad67/NoteCollector_V2/service/impl/JWTServiceIMPL.java
@@ -1,12 +1,23 @@
 package lk.ijse.gdse.aad67.NoteCollector_V2.service.impl;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lk.ijse.gdse.aad67.NoteCollector_V2.service.JWTService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.function.Function;
+@Service
 public class JWTServiceIMPL implements JWTService {
+    @Value("${spring.jwtKey}")
+    String jwtKey;
     @Override
     public String extractUserName(String token) {
-        return "";
+        return extractClaim(token, Claims::getSubject);
     }
 
     @Override
@@ -22,5 +33,18 @@ public class JWTServiceIMPL implements JWTService {
     @Override
     public String refreshToken(String prevToken) {
         return "";
+    }
+    private <T> T extractClaim(String token, Function<Claims,T> claimsResolve) {
+       final Claims claims = getClaims(token);
+       return claimsResolve.apply(claims);
+    }
+
+    private Claims getClaims(String token) {
+       return Jwts.parser().setSigningKey(getKey()).build().parseClaimsJwt(token)
+                .getBody();
+    }
+    private Key getKey() {
+        byte [] decodedJWT = Decoders.BASE64.decode(jwtKey);
+        return Keys.hmacShaKeyFor(decodedJWT);
     }
 }
