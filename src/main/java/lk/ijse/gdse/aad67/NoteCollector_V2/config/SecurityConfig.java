78534841e31a1e156c441980ec2f0c5aaf6ddc43
ThanuraSilva <thanurasilva1989@gmail.com>
2024-10-12 12:30:10 +0530

    On procesin the SecurityConfig add permissions, authenticationProvider (not implemented) and addFilterBefore (filler placed, but not impl yet)

--- src/main/java/lk/ijse/gdse/aad67/NoteCollector_V2/config/SecurityConfig.java
+++ src/main/java/lk/ijse/gdse/aad67/NoteCollector_V2/config/SecurityConfig.java
@@ -1,5 +1,22 @@
 package lk.ijse.gdse.aad67.NoteCollector_V2.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+public class SecurityConfig {
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers("api/v1/auth/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider()
+                .addFilterBefore()
+    }
 }
